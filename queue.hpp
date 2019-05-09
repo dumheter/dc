1c760fdd997b0a762f313def77fb54e847c44d0a
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-05-09 20:21:45 +0200

    New file, Queue.hpp. A queue, implemented as a ring buffer ontop of std::vector.

---
+++ queue.hpp
@@ -0,0 +1,214 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2019 Christoffer Gustafsson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef QUEUE_HPP_
+#define QUEUE_HPP_
+
+#include <mutex>
+#include <vector>
+#include "types.hpp"
+
+namespace dutil {
+
+enum class QueueResult : u8 { kSuccess = 0, kFull, kEmpty };
+
+constexpr bool kNoMutex = false;
+constexpr bool kUseMutex = true;
+
+/**
+ * Queue implemented with a ring buffer using std::vector as container.
+ *
+ * @tparam T The type the queue will hold
+ * @tparam TUseMutex will decide if the class will be thread safe.
+ *
+ * TODO replace some mutex locks with atomic CAS operations for better perf.
+ */
+template <typename T, bool TUseMutex = kUseMutex>
+class Queue {
+ public:
+  /**
+   * @param size Note, with a size of 256 you can store 255 elements.
+   */
+  explicit Queue(const size_t size = 256);
+
+  /**
+   * Insert @elem at the end of the queue.
+   */
+  QueueResult Push(const T& elem);
+  QueueResult Push(T&& elem);
+
+  /**
+   * Remove the first element in the queue and put it in @dataOut.
+   */
+  QueueResult Pop(T& dataOut);
+
+  /**
+   * Is the queue empty?
+   */
+  bool Empty() {
+    if constexpr (TUseMutex) {
+      std::lock_guard<std::mutex> lock(mutex_);
+      return queue_position_.empty();
+    } else {
+      return queue_position_.empty();
+    }
+  }
+
+  /**
+   * How many elements are in the queue.
+   */
+  size_t Size() {
+    if constexpr (TUseMutex) {
+      std::lock_guard<std::mutex> lock(mutex_);
+      return queue_position_.getSize();
+    } else {
+      return queue_position_.getSize();
+    }
+  }
+
+ private:
+  std::vector<T> vector_;
+  std::mutex mutex_{};
+
+  class QueuePosition {
+   public:
+    /**
+     * As of now, when setting totalSize, you can store totalSize-1 elements.
+     */
+    QueuePosition(size_t totalSize)
+        : front_(0), back_(0), totalSize_(totalSize) {}
+
+    inline QueueResult AddBack() {
+      const size_t tmp = back_;
+      back_ = back_ + 1 < totalSize_ ? back_ + 1 : 0;
+      QueueResult res;
+      if (back_ != front_) {
+        res = QueueResult::kSuccess;
+      } else {
+        back_ = tmp;
+        res = QueueResult::kFull;
+      }
+      return res;
+    }
+
+    inline QueueResult RemoveFront() {
+      QueueResult res;
+      if (front_ != back_) {
+        front_ = front_ + 1 < totalSize_ ? front_ + 1 : 0;
+        res = QueueResult::kSuccess;
+      } else {
+        res = QueueResult::kEmpty;
+      }
+      return res;
+    }
+
+    inline size_t back() const { return back_; }
+
+    inline size_t front() const { return front_; }
+
+    inline bool Empty() const { return front_ == back_; }
+
+    inline size_t GetSize() const {
+      if (back_ >= front_)
+        return back_ - front_;
+      else
+        return totalSize_ - front_ + back_;
+    }
+
+   private:
+    size_t front_;
+    size_t back_;
+    size_t totalSize_;
+  } queue_position_;
+};
+
+// ============================================================ //
+// Template Definition
+// ============================================================ //
+
+template <typename T, bool TUseMutex>
+Queue<T, TUseMutex>::Queue(size_t size)
+    : vector_(size), queue_position_(size) {}
+
+template <typename T, bool TUseMutex>
+QueueResult Queue<T, TUseMutex>::Push(const T& elem) {
+  if constexpr (TUseMutex == kUseMutex) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    const QueueResult res = this->queue_position_.AddBack();
+    if (res == QueueResult::kSuccess) {
+      vector_[this->queue_position_.back()] = elem;
+    }
+    return res;
+  } else {
+    const QueueResult res = this->queue_position_.AddBack();
+    if (res == QueueResult::kSuccess) {
+      vector_[this->queue_position_.back()] = elem;
+    }
+    return res;
+  }
+}
+
+template <typename T, bool TUseMutex>
+QueueResult Queue<T, TUseMutex>::Push(T&& elem) {
+  if constexpr (TUseMutex == kUseMutex) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    const QueueResult res = this->queue_position_.AddBack();
+    if (res == QueueResult::kSuccess) {
+      vector_[this->queue_position_.back()] = std::move(elem);
+    }
+    return res;
+  } else {
+    const QueueResult res = this->queue_position_.AddBack();
+    if (res == QueueResult::kSuccess) {
+      vector_[this->queue_position_.back()] = std::move(elem);
+    }
+    return res;
+  }
+}
+
+template <typename T, bool TUseMutex>
+QueueResult Queue<T, TUseMutex>::Pop(T& dataOut) {
+  QueueResult res;
+  if constexpr (TUseMutex == kUseMutex) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (!this->queue_position_.Empty()) {
+      res = this->queue_position_.RemoveFront();
+      dataOut = std::move(vector_[queue_position_.front()]);
+    } else {
+      res = QueueResult::kEmpty;
+    }
+  } else {
+    if (!this->queue_position_.Empty()) {
+      res = this->queue_position_.RemoveFront();
+      dataOut = std::move(vector_[queue_position_.front()]);
+    } else {
+      res = QueueResult::kEmpty;
+    }
+  }
+  return res;
+}
+
+}  // namespace dutil
+
+#endif  // QUEUE_HPP_
