34799adceb624c67c153939b9c6bf1ffe8052202
Christoffer Gustafsson <christoffer.golf@gmail.com>
2021-08-16 23:09:33 +0200

    Removed the format ps script, now using a global function instead.

--- psformat.ps1
+++
@@ -1,9 +0,0 @@
-$files = Get-ChildItem -Path . -File -Recurse -Include "*.hpp","*.cpp","*.h" |
-where {
-      $_.FullName.Contains("dc\include") -or
-      $_.FullName.Contains("dc\src") -or
-      $_.FullName.Contains("dc\tests")
-}
-
-foreach ($file in $files) { clang-format -style=file -i $file.FullName } 
-echo "Done formatting all .(cpp|h|hpp) files."
