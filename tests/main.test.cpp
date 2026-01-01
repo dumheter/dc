52c3dc4c59a8034a0a02ec8b00ad7364d6ee693d
Christoffer Gustafsson <5116410+dumheter@users.noreply.github.com>
2026-01-01 18:26:19 +0100

    .

--- tests/main.test.cpp
+++ tests/main.test.cpp
@@ -1,24 +1,4 @@
-#include <elfutils/libdw.h>
-
 #include <dc/dtest.hpp>
 #include <dc/fmt.hpp>
 
-#include "dc/assert.hpp"
-#include "dc/callstack.hpp"
-
-void abc() {
-  auto result = dc::buildCallstack();
-  // DC_ASSERT(result.isErr(), "my assert message");
-  DC_FATAL_ASSERT(result.isOk(), "my fatal assert message");
-  LOG_INFO("{}", result->callstack);
-}
-
-// int main(int argc, char** argv) { return DTEST_RUN(); }
-int main(int, char**) {
-  dc::log::init();
-
-  abc();
-
-  dc::log::deinit();
-  return 0;
-}
+int main(int argc, char** argv) { return DTEST_RUN(); }
